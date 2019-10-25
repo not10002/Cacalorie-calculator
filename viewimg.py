4e3208b25f72b17c2658409124354eba9a587ab6
it62070023 <50399016+not10002@users.noreply.github.com>
2019-10-25 15:22:29 +0700

    Add files via upload

--- viewimg.py
+++ viewimg.py
@@ -1,14 +1,19 @@
 import cv2
 import numpy as np
-
-img = cv2.imread("1.jpg", -1)
+def nothing(x):
+    pass
+img = cv2.imread("2.jpg")
 while True:
-    color = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.imshow("image", img)
-    l_b = np.array([110, 50, 50])
-    u_b = np.array([130, 255, 255])
-    mask = cv2.inRange(color, l_b, u_b)
-    cv2.imshow("mask", mask)
-    if cv2.waitKey(0) & 0xFF==ord('q'):
+    cv2.imshow("img", img)
+    color = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    lower = np.array([50, 225, 220])
+    upper = np.array([255, 255, 255])
+
+    mask = cv2.inRange(color, lower, upper)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow("color", color)
+    cv2.imshow("oh", res)
+    if cv2.waitKey(0):
         break
     cv2.destroyAllWindows()
