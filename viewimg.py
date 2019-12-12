a9b9a4226373bc98436d8fd87632a8dc3bbd2727
it62070023 <50399016+not10002@users.noreply.github.com>
2019-12-12 20:11:02 +0700

    Add files via upload

--- viewimg.py
+++ viewimg.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 def nothing(x):
     pass
-def main():
-    img = cv2.imread("test.jpg") #image read
+def main(myfile):
+    img = cv2.imread(myfile) #image read
     while True:
         img = cv2.resize(img, (500,500)) #window size
         cv2.imshow("img", img) #show image
         color = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) #change type color
 
-        lower = np.array([110, 0, 100]) # ค่าของสีที่น้อยที่สุด
+        lower = np.array([0, 85, 0]) # ค่าของสีที่น้อยที่สุด
         upper = np.array([255, 255, 255]) # ค่าของสีที่มากที่สุด
 
         mask = cv2.inRange(color, lower, upper) #แสดงช่วงสีนั้น
